961af025952e6a7e37869ecb7167f348c3e92e36
Sumit Yadav <careeratedjing@gmail.com>
2021-12-05 18:28:26 +0530

    basic dep changes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_restful import Api
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 from db import db
 from resources.home import Home
@@ -9,9 +10,10 @@ from resources.url_shortner import URLShortner
 from resources.url import URL
 from resources.search import Search
 
-load_dotenv()
+# load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 # configs will be placed here
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -30,4 +32,5 @@ api.add_resource(URL, '/<short_url>')
 
 if __name__ == '__main__':
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
+    # app.run(port=5000, debug=True)
